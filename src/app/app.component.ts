cdc38cf81b71d90d39be1587fba4dd9f37c8ca8a
Nguyễn Quốc Khánh <martinjarvis292@gmail.com>
2022-02-23 08:39:20 +0700

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,26 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'prob1';
+
+  items: any[] = [];
+
+  newTask : String = ''; 
+  // complete: any = document.getElementsByTagName("li");
+  
+  toggle = true;
+  status = "";
+
+  addToList(): void {
+    this.items.push(this.newTask);
+    this.newTask = '';
+  }
+
+  removeFromList(): void {
+    this.items.shift();
+  }
+
+  completed(): void {
+    this.toggle = !this.toggle;
+    this.status = this.toggle ? "" : "Complete";
+  }
 }
